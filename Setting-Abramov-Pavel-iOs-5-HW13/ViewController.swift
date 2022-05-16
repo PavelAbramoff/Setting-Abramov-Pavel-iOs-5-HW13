c1e734318bb2a8db15a20f43fbb8b95124265ce5
PavelAbramoff <96954222+PavelAbramoff@users.noreply.github.com>
2022-05-16 09:40:03 +0400

    Initial Commit

---
+++ Setting-Abramov-Pavel-iOs-5-HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting-Abramov-Pavel-iOs-5-HW13
+//
+//  Created by Pavel Абрамов on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
